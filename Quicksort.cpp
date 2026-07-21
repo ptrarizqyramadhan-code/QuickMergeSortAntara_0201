cea94a714bbc2b3651f219826e27c7919b230f61
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-21 15:22:42 +0700

    step 1

---
+++ Quicksort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;  
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
